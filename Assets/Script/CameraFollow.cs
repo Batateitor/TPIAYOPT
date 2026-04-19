21f2f82b758791e266d71aa9fbb98517f62c6652
Batateitor <facubats@hotmail.com.ar>
2026-04-19 19:07:30 -0300

    Más chuches
    hola robbert =)

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    [SerializeField] public float fixedY = 12f;
+    [SerializeField] public float offsetZ = 10f;
+    public Vector3 rotation = new Vector3(55f, 0f, 0f);
+
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = new Vector3(
+            target.position.x,
+            fixedY,
+            target.position.z + offsetZ
+        );
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
